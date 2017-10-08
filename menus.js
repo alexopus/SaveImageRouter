dfd991201b3cb2deea5ba15b5babb35113f3f02c
alexopus <alexopus.soft@gmail.com>
2017-10-08 21:43:06 +0200

    let Chrome determine the initial filename

--- menus.js
+++ menus.js
@@ -1,12 +1,7 @@
+var downloadInfo = {};
+
 function saveImageTo(info, tab) {
     var url = info.srcUrl;
-    var a = document.createElement('a');
-    a.href = info.pageUrl;
-    var pageUrl = a.hostname;
-    var path_ = a.pathname.replace(new RegExp('/', 'g'), '_');
-    var filename = url.substring(url.lastIndexOf('/') + 1);
-    var ext = filename.substr(filename.lastIndexOf('.') + 1);
-    var filename = filename.substring(0, filename.lastIndexOf('.'));
 
     chrome.storage.local.get({
         entries: []
@@ -15,86 +10,30 @@ function saveImageTo(info, tab) {
             return e.title == info.menuItemId;
         });
 
-        var title = items.entries[index].title.trim();
-        var directory = items.entries[index].directory.trim();
+        downloadInfo.directory = items.entries[index].directory.trim();
         var dialog = items.entries[index].dialog;
         var ifExists = items.entries[index].ifExists;
 
         if ("rename" in items.entries[index]) {
-            var date = new Date();
-            var dateStr = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + "-" +  date.getHours() + "-" + date.getMinutes() + "-" + date.getSeconds();
-
-            switch (items.entries[index].rename.name) {
-                case 'keep original name':
-                    break;
-                case '"SaveImageRouter"':
-                    filename = "SaveImageRouter";
-                    break;
-                case 'empty name':
-                    filename = "";
-                    break;
-                default:
-                    break;
-            }
-
-            switch (items.entries[index].rename.prefix) {
-                case 'no prefix':
-                    break;
-                case 'timestamp prefix':
-                    filename = dateStr + '_' + filename;
-                    break;
-                case 'host prefix':
-                    filename = pageUrl + '_' + filename;
-                    break;
-                case 'URL path':
-                    filename = path_ + '_' + filename;
-                    break;
-                default:
-                    break;
-            }
-
-            switch (items.entries[index].rename.suffix) {
-                case 'no suffix':
-                    break;
-                case 'timestamp suffix':
-                    filename = filename + '_' + dateStr;
-                    break;
-                case 'host suffix':
-                    filename = filename + '_' + pageUrl;
-                    break;
-                case 'URL path':
-                    filename = filename + '_' + path_;
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        if (filename == "") {
-            filename = "SaveImageRouter";
-        }
-        filename = filename + "." + ext;
-
-        if (directory === "") {
-            // base directory, do not add slash
-            filename = directory + filename;
+            downloadInfo.rename = true;
+            downloadInfo.renameName = items.entries[index].rename.name;
+            downloadInfo.renamePrefix = items.entries[index].rename.prefix;
+            downloadInfo.renameSuffix = items.entries[index].rename.suffix;
         } else {
-            filename = directory + "/" + filename;
+            downloadInfo.rename = false;
         }
 
-        filename = filename.replace(/[\#\?].*$/,'');
-
-        var conflictAction = "uniquify";
+        downloadInfo.conflictAction  = "uniquify";
 
         switch (ifExists) {
             case "rename":
-                conflictAction = "uniquify";
+                downloadInfo.conflictAction =  "uniquify";
                 break;
             case "overwrite":
-                conflictAction = "overwrite";
+                downloadInfo.conflictAction = "overwrite";
                 break;
             case "dialog":
-                conflictAction = "prompt";
+                downloadInfo.conflictAction = "prompt";
                 break;
             default:
                 break;
@@ -102,9 +41,9 @@ function saveImageTo(info, tab) {
 
         chrome.downloads.download({
             url: url,
-            filename: filename,
-            conflictAction: conflictAction,
             saveAs: dialog
+        }, function(downloadId) {
+            downloadInfo.id = downloadId;
         });
     });
 }
@@ -113,9 +52,11 @@ function saveToDefault(info, tab) {
     var url = info.srcUrl;
     chrome.downloads.download({
         url: url,
-        filename: url.substring(url.lastIndexOf('/') + 1),
         conflictAction: "uniquify",
         saveAs: false
+    }, function(downloadId) {
+        downloadInfo.id = downloadId;
+        downloadInfo.directory = '_default_';
     });
 }
 
@@ -150,10 +91,107 @@ function createMenus() {
 
 createMenus();
 
+chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
+    if (item.id === downloadInfo.id) {
+        if (downloadInfo.directory === '_default_') {
+            // do not suggest anything, use defaults.
+        } else {
+            // get host name and file path
+            var a = document.createElement('a');
+            a.href = item.url;
+            var hostname = a.hostname;
+            var path_ = a.pathname.replace(new RegExp('/', 'g'), '_');
+            // get filename determined by Chrome
+            var filename = item.filename;
+            // separate extension from the filename
+            var ext = filename.substr(filename.lastIndexOf('.') + 1);
+            filename = filename.substring(0, filename.lastIndexOf('.'));
+            // get directory from the saved download info
+            var directory = downloadInfo.directory;
+
+            // do renaming of the filename if required
+            if (downloadInfo.rename) {
+                var date = new Date();
+                var dateStr = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + "-" +  date.getHours() + "-" + date.getMinutes() + "-" + date.getSeconds();
+
+                switch (downloadInfo.renameName) {
+                    case 'keep original name':
+                        break;
+                    case '"SaveImageRouter"':
+                        filename = "SaveImageRouter";
+                        break;
+                    case 'empty name':
+                        filename = "";
+                        break;
+                    default:
+                        break;
+                }
+
+                switch (downloadInfo.renamePrefix) {
+                    case 'no prefix':
+                        break;
+                    case 'timestamp prefix':
+                        filename = dateStr + '_' + filename;
+                        break;
+                    case 'host prefix':
+                        filename = hostname + '_' + filename;
+                        break;
+                    case 'URL path':
+                        filename = path_ + '_' + filename;
+                        break;
+                    default:
+                        break;
+                }
+
+                switch (downloadInfo.renameSuffix) {
+                    case 'no suffix':
+                        break;
+                    case 'timestamp suffix':
+                        filename = filename + '_' + dateStr;
+                        break;
+                    case 'host suffix':
+                        filename = filename + '_' + hostname;
+                        break;
+                    case 'URL path':
+                        filename = filename + '_' + path_;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            // make sure renaming didn't erase the filename completely (no prefix + empty name + no suffix)
+            if (filename == "") {
+                filename = "SaveImageRouter";
+            }
+            // add back the extension to the filename
+            filename = filename + "." + ext;
+
+            // prepend the directory to the modified filename
+            if (directory === "") {
+                // base directory, do not add slash
+                filename = directory + filename;
+            } else {
+                filename = directory + "/" + filename;
+            }
+
+            // make sure the modified filename doesn't contain any illegal characters
+            filename = filename.replace(/[\#\?].*$/,'');
+
+            // suggest the new filename to Chrome
+            suggest({ filename: filename, conflictAction: downloadInfo.conflictAction });
+        }
+    }
+
+    // reset the download info for the next download item
+    downloadInfo = {};
+});
+
 // update menus when new entries are saved from the options
 chrome.storage.onChanged.addListener(function(changes, namespace) {
-    chrome.contextMenus.removeAll();
-    createMenus();
+    chrome.contextMenus.removeAll(function() {
+        createMenus();
+    });
 });
 
 function createEntriesMenus(items) {
